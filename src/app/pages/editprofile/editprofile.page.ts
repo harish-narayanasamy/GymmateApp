bd39eb7eab849d338dd75fe0b569901f9642fbc6
Harish Narayanasamy <h.narayanasamy@lakeba.com>
2020-03-30 09:28:38 +1100

    formdata  save

--- src/app/pages/editprofile/editprofile.page.ts
+++ src/app/pages/editprofile/editprofile.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../services/data.service';
+import { FirebaseService } from '../../services/firebase.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { strings } from '../../config/strings';
 import {StreamingMedia, StreamingVideoOptions} from '@ionic-native/streaming-media/ngx';
 import { AdmobService } from '../../services/admob.service';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -14,9 +15,24 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 })
 export class EditprofilePage implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: FirebaseService) { }
 
   ngOnInit() {
   }
+  onSubmit(f: NgForm) {
+    console.log(f)
+    this.dataService.postData(f.value.name, f.value.age, f.value.gender,f.value.Height,f.value.Weight)
+      .then(res => {
 
+        f.reset();
+        alert(res)
+        console.log("start2")
+
+      }, err => {
+        console.log("start3")
+
+       alert(err)
+      })
+
+  }
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,9 @@ export class FirebaseService {
 
   constructor(
     public afs: AngularFirestore,
-    public afAuth: AngularFireAuth
-  ) {}
+    public afAuth: AngularFireAuth,
+    public afDb: AngularFireDatabase
+  ) { }
 
   unsubscribeOnLogOut() {
     // remember to unsubscribe from the snapshotChanges
@@ -23,16 +25,32 @@ export class FirebaseService {
 
   getDisplayName() {
     const user = firebase.auth().currentUser;
-    if ( user != null) {
+    if (user != null) {
       return user.displayName;
     }
   }
 
   getCreationTime() {
     const user = firebase.auth().currentUser;
-    if ( user != null) {
+    if (user != null) {
       return user.metadata.creationTime;
     }
   }
+  postData(name, age, gender, height, weight) {
+    console.log(name)
+    console.log(age)
+    console.log(gender)
+    console.log(height)
+    console.log(weight)
 
+    return new Promise<any>((resolve, reject) => {
+      this.afDb.database.ref('posts/').push({
+        name: name, age: age, gender: gender, height: height, weight: weight
+      })
+        .then(res => {
+          resolve(res);
+        },
+          err => reject(err))
+    })
+  }
 }
