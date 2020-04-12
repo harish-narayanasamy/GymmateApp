73ec7d3d63e66a5af7dc58b9dbfd3cb61634e4d5
Harish Narayanasamy <h.narayanasamy@lakeba.com>
2020-04-12 16:33:39 +1000

    final2

--- src/app/pages/weday/weday.page.ts
+++ src/app/pages/weday/weday.page.ts
@@ -5,7 +5,8 @@ import { DataService } from '../../services/data.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { AdmobService } from '../../services/admob.service';
-
+import {StreamingMedia, StreamingVideoOptions} from '@ionic-native/streaming-media/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 @Component({
   selector: 'app-weday',
   templateUrl: './weday.page.html',
@@ -13,6 +14,8 @@ import { AdmobService } from '../../services/admob.service';
 })
 export class WedayPage implements OnInit {
 
+
+  enabledID = null;
   public strings = strings;
   exercises: ExercisesObject[] = [];
   day: any;
@@ -37,8 +40,9 @@ export class WedayPage implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     public plt: Platform,
-    public admob: AdmobService,
-
+    private streamingMedia: StreamingMedia,
+    private admob: AdmobService,
+    private screenOrientation: ScreenOrientation,
     ) {}
 
     ngOnInit() {
@@ -83,15 +87,36 @@ export class WedayPage implements OnInit {
 }
 
 
-toggleInfo(index: number) {
-  this.show[index] = true;
+toggleInfo(index: number,id) {
+
+  if(this.enabledID==index){
+    this.enabledID =null;
+
+  }else{
+    this.dataService.getDataExerciseById(id)
+    .subscribe( resp => {
+
+      this.exercise2 = resp[0];
+      this.isLoading = false;
+
+  } );
+    this.show[index] = true;
+    this.enabledID =index;
+  }
+ 
 }
 
- toggleInfo(y: boolean) {
-  this.show[y] = true;
+play() {
+  const options: StreamingVideoOptions = {
+    successCallback: () => { console.log('Video played'); },
+    errorCallback: (e) => { console.log('Error streaming'); },
+    orientation: 'landscape',
+    shouldAutoClose: true,
+    controls: true
+  };
+
+  // tslint:disable-next-line: max-line-length
+  this.streamingMedia.playVideo('https://firebasestorage.googleapis.com/v0/b/six-pack-app.appspot.com/o/video.mp4?alt=media&token=453512dd-f079-4355-a325-92a9b2f7e465', options);
 }
 
-hide() {
-  this.hideMe = true;
-}
 }
