4a8aa3e5763d50345dce8644ae46279f987b6b64
subarna124 <49214617+subarna124@users.noreply.github.com>
2020-04-17 12:47:19 +1000

    diet

---
+++ src/app/app.module.ts
@@ -0,0 +1,74 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { PlanPage }     from './modals/plan/plan.page';
+
+import { SubscriptionPage } from './modals/subscription/subscription.page';
+import { EditprofilePage }     from './pages/editprofile/editprofile.page';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+
+
+import { firebaseconfig } from './config/firebase';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AuthService } from './services/auth.service';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import {ScrollingHeaderModule} from 'ionic-scrolling-header';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import {StreamingMedia} from '@ionic-native/streaming-media/ngx';
+import { AdMobFree } from '@ionic-native/admob-free/ngx';
+import { AdmobService } from './services/admob.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent,PlanPage,SubscriptionPage,],
+  entryComponents: [PlanPage,SubscriptionPage],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ScrollingHeaderModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseconfig.firebase), // imports firebase/app
+    AngularFirestoreModule, // imports firebase/firestore
+    AngularFireAuthModule, // imports firebase/auth
+    AngularFireStorageModule, // imports firebase/storage
+    AngularFireDatabaseModule, BrowserAnimationsModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    PlanPage,
+    Facebook,
+    AuthService,
+    StreamingMedia,
+    AdMobFree,
+    AdmobService,
+    InAppBrowser,
+    ScreenOrientation,
+    { provide: FirestoreSettingsToken, useValue: {} },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
