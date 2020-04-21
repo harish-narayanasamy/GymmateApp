e62ab1e5a12b7e2a7043d51d768c804d4b294bf2
Harish Narayanasamy <h.narayanasamy@lakeba.com>
2020-04-21 16:19:57 +1000

    changes in workout

--- src/app/pages/wdetails/wdetails.page.ts
+++ src/app/pages/wdetails/wdetails.page.ts
@@ -11,79 +11,105 @@ import { FavoriteService } from '../../services/favorite.service';
   styleUrls: ['./wdetails.page.scss'],
 })
 export class WdetailsPage implements OnInit {
-  show: {[key: number]: boolean} = {};
+  show: { [key: number]: boolean } = {};
   public strings = strings;
   workout: any = {};
   isLoading = false;
   id: any;
   innerHeight: any;
   days = new Array(7);
+  days2: any
   icon = 'star-outline';
-  
+  subscribe = true;
   constructor(
     private dataService: DataService,
     private router: Router,
     private route: ActivatedRoute,
     public plt: Platform,
     private dataFavorite: FavoriteService
-    ) {}
+  ) { }
 
-    exercise: any = {};
-    isInfoHidden = true;
-    isInstruHidden = true;
-    isTipsHidden = true;
-    orientation = 'landscape';
+  exercise: any = {};
+  isInfoHidden = true;
+  isInstruHidden = true;
+  isTipsHidden = true;
+  orientation = 'landscape';
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
+  isPositive(x: string, y: number): boolean {
+    return (parseInt(x) === y);
+  }
+  async ionViewWillEnter() {
+    this.isLoading = true;
+    this.innerHeight = this.plt.height() / 3 + 'px';
+    this.route.params.subscribe(
+      data => {
+        this.id = data.id;
+        this.getDays();
+        this.getDetails();
 
-    async ionViewWillEnter() {
-      this.isLoading = true;
-      this.innerHeight = this.plt.height() / 3 + 'px';
-      this.route.params.subscribe(
-        data => {
-          this.id = data.id;
-          this.getDetails();
+        this.dataFavorite.existWorkout(this.id)
+          .then(exist => this.icon = (exist) ? 'star' : 'star-outline');
 
-          this.dataFavorite.existWorkout(this.id)
-          .then( exist => this.icon = ( exist ) ? 'star' : 'star-outline');
-
-          if (!this.id) {
-            this.goBack();
-          }
+        if (!this.id) {
+          this.goBack();
         }
-      );
-    }
+      }
+    );
+  }
 
-    goBack() {
-      this.router.navigate(['/home']);
-    }
+  goBack() {
+    this.router.navigate(['/home']);
+  }
 
   getDetails() {
 
     this.dataService.getDataWorkoutById(this.id)
-    .subscribe( resp => {
+      .subscribe(resp => {
 
-      this.workout = resp[0];
-      this.isLoading = false;
 
-  } );
-}
 
-addToFavorites() {
-  const workout = {
-    id: this.workout.workout_id,
-    title: this.workout.workout_title,
-    image: this.workout.workout_image,
-};
+        this.workout = resp[0];
+        this.isLoading = false;
 
-  const exist = this.dataFavorite.saveWorkout( workout );
-  this.icon = ( exist ) ? 'star' : 'star-outline';
-}
+      });
+  }
+  getDays() {
 
-toggleInfo(index: number) {
-  this.show[index] = true;
-}
+    this.dataService.getDataNumberDaysById(this.id)
+      .subscribe(resp => {
+
+        console.log(resp[0])
+        this.days2 = resp;
+        for (let i in this.days2) {
+          console.log(this.days2[i])
+        }
+        this.isLoading = false;
+
+      });
+  }
+  checkavail(y: number) {
+    for (let i in this.days2) {
+      if (parseInt(this.days2[i].day) === y)
+        return true
+    }
+  }
+
+  addToFavorites() {
+    const workout = {
+      id: this.workout.workout_id,
+      title: this.workout.workout_title,
+      image: this.workout.workout_image,
+    };
+
+    const exist = this.dataFavorite.saveWorkout(workout);
+    this.icon = (exist) ? 'star' : 'star-outline';
+  }
+
+  toggleInfo(index: number) {
+    this.show[index] = true;
+  }
 
 
 
